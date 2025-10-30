1d72fdbca0c51c6eb13d0f301d029a652da9faf0
EverythingHasBSOD <qasimlimurad92@gmail.com>
2025-10-30 22:16:03 +0400

    commit all

---
+++ app.js
@@ -0,0 +1,11 @@
+function messagefuckingasshole() {
+   alert('Sorry. No Downloads Availble!');
+}
+
+window.onload = function(){
+  // your code here
+      alert("LANGUAGE WARNING!");
+};
+
+let YOUR_MOTHERFUCKING_PC
+
